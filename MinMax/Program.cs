08abda1b080f7fabdb59286b13282fe93eb8b542
minhvu1502 <43747199+minhvu1502@users.noreply.github.com>
2020-05-15 22:01:21 +0700

    Add project files.

---
+++ MinMax/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. TCP listener, min max 2 so
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.1"), 1234);
+            TcpListener listener = new TcpListener(iPEndPoint);
+            listener.Start();
+            Socket sk = listener.AcceptSocket();
+
+            NetworkStream ns = new NetworkStream(sk);
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+
+            String dl = sr.ReadLine();
+            Console.WriteLine(dl);
+
+            sw.WriteLine("hello client");
+            sw.Flush();
+            while (true)
+            {
+                String result;
+                String haiSo = sr.ReadLine();
+                if (haiSo.Equals("bye"))
+                {
+                    Console.WriteLine("exit");
+                    break;
+                }
+                Int16 a = Int16.Parse(haiSo.Split()[0]);
+                Int16 b = Int16.Parse(haiSo.Split()[1]);
+                if (a > b)
+                {
+                    result = "Max: " + a + ", Min: " + b;
+                }
+                else
+                {
+                    result = "Max: " + b + ", Min: " + a;
+                }
+                Console.WriteLine(result);
+                sw.WriteLine(result);
+                sw.Flush();
+            }
+            sw.Close();
+            sr.Close();
+            ns.Close();
+            sk.Close();
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ client/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket sk = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint dcsv = new IPEndPoint(IPAddress.Parse("127.0.1"), 1234);
+            sk.Connect(dcsv);
+            NetworkStream ns = new NetworkStream(sk);
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.WriteLine("hello server");
+            sw.Flush();
+
+            Console.WriteLine(sr.ReadLine());
+
+            while (true)
+            {
+                Console.WriteLine("Nhap vao 2 so:");
+                string data = Console.ReadLine();
+                sw.WriteLine(data);
+                sw.Flush();
+                if (data.Equals("bye"))
+                {
+                    Console.WriteLine("exit");
+                    break;
+                }
+                Console.WriteLine(sr.ReadLine());
+
+            }
+            sw.Close();
+            sr.Close();
+            ns.Close();
+            sk.Close();
+
+            Console.ReadLine();
+
+        }
+    }
+}
